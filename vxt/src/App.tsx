51dcfbce666b052d6ac03ea496b5032ee5fde924
JosipSankovic <sankovic52@gmail.com>
2026-04-14 22:41:27 +0200

    feat(routing): add ScrollToTop component to reset scroll on route change

--- vxt/src/App.tsx
+++ vxt/src/App.tsx
@@ -1,18 +1,30 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ContactPage from './pages/ContactPage'
 import ServiceHubPage from './pages/ServiceHubPage'
 import SubServicePage from './pages/SubServicePage'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/o-nama" element={<AboutPage />} />
-      <Route path="/kontakt" element={<ContactPage />} />
-      <Route path="/usluge" element={<ServiceHubPage />} />
-      <Route path="/usluge/:slug" element={<SubServicePage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/o-nama" element={<AboutPage />} />
+        <Route path="/kontakt" element={<ContactPage />} />
+        <Route path="/usluge" element={<ServiceHubPage />} />
+        <Route path="/usluge/:slug" element={<SubServicePage />} />
+      </Routes>
+    </>
   )
 }
